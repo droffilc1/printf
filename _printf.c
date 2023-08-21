5e7f439dec07e625f5c791bc5921f154bc532e5a
SherneVK <shernekinniar@gmail.com>
2023-08-21 08:43:03 -0700

    pf

--- _printf.c
+++ _printf.c
@@ -12,43 +12,45 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i;
-    int printed_chars = 0;
+	va_list args;
+	int i;
+	int printed_chars = 0;
+	char c;
+	char *str;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                printed_chars++;
-            }
-            else if (format[i] == 's')
-            {
-                char *str = va_arg(args, char *);
-                fputs(str, stdout);
-                printed_chars += strlen(str);
-            }
-            else if (format[i] == '%')
-            {
-                putchar('%');
-                printed_chars++;
-            }
-            /* can add more conversion specifiers here if needed */
-        }
-        else
-        {
-            putchar(format[i]);
-            printed_chars++;
-        }
-    }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				putchar(c);
+				printed_chars++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				fputs(str, stdout);
+				printed_chars += strlen(str);
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				printed_chars++;
+			}
+			/* can add more conversion specifiers here if needed */
+		}
+		else
+		{
+			putchar(format[i]);
+			printed_chars++;
+		}
+	}
 
-    va_end(args);
-    return printed_chars;
+	va_end(args);
+	return (printed_chars);
 }
