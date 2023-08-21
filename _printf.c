1c45e83afbfdc013396569beab89f821a53eacbe
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2023-08-21 21:40:57 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,8 +10,6 @@
  *
  * Return: Number of characters printed
  */
-<<<<<<< HEAD
-
 int _printf(const char *format, ...)
 {
 	int count;
@@ -34,45 +32,4 @@ int _printf(const char *format, ...)
 	count = get_fmt(format, specs, ap);
 	va_end(ap);
 	return (count);
-=======
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int i;
-	int printed_chars = 0;
-
-	va_start(args, format);
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				/* Handle 'c'*/
-			}
-			else if (format[i] == 's')
-			{
-				/* Handle 's' */
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				printed_chars += handle_decimal(args); /* Call integer handler */
-			}
-			else if (format[i] == '%')
-			{
-				/* Handle '%%' */
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			printed_chars++;
-		}
-	}
-
-	va_end(args);
-	return (printed_chars);
->>>>>>> 8a0d0b1e0fd5883fa2ae81c57e3463f6a0c1e340
 }
