74e8e2889ff11d540072aeec474fed0c4b452d32
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-21 21:53:41 +0300

    Add solution

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -28,16 +27,4 @@ int handle_char(va_list ap);
 int handle_string(va_list ap);
 int handle_integer(va_list ap);
 int handle_percent(__attribute__((unused))va_list ap);
-=======
-#include <stdarg.h>
-
-int _putchar(char c);
-void _puts(char *str);
-void print_number(int n);
-void print_binary(unsigned int n);
-int handle_conversion(const char *format, int *i, va_list args);
-int _printf(const char *format, ...);
-int handle_decimal(va_list args);
-
->>>>>>> 8a0d0b1e0fd5883fa2ae81c57e3463f6a0c1e340
 #endif /* MAIN_H */
