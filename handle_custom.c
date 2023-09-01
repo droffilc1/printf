8687b5036fd040c4fdb9e53c24f578c822afe9a5
SherneVK <shernekinniar@gmail.com>
2023-09-01 05:34:57 -0700

    handle custom 12

--- handle_custom.c
+++ handle_custom.c
@@ -9,31 +9,40 @@
 int handle_custom_string(va_list ap)
 {
 	char *str = va_arg(ap, char *);
-	int tel = 0;
+	int count = 0;
+
+	char hex1, hex2;
 
 	if (str == NULL)
 	{
 		return (_printf("(null)"));
 	}
 
-	if (str[0] == '%' && str[1] == 'S')
-	{
-		str += 2;
-	}
-
 	for (; *str != '\0'; str++)
 	{
 		if (*str < 32 || *str >= 127)
 		{
-			tel += _putchar('\\');
-			tel += _putchar('x');
-			tel += print_hexadecimal(*str, 1);
+			hex1 = (*str >> 4) & 0xF;
+			hex2 = *str & 0xF;
+
+			count += _putchar('\\');
+			count += _putchar('x');
+
+			if (hex1 < 10)
+				count += _putchar(hex1 + '0');
+			else
+				count += _putchar(hex1 - 10 + 'A');
+
+			if (hex2 < 10)
+				count += _putchar(hex2 + '0');
+			else
+				count += _putchar(hex2 - 10 + 'A');
 		}
 		else
 		{
-			tel += _putchar(*str);
+			count += _putchar(*str);
 		}
 	}
 
-	return (tel);
+	return (count);
 }
