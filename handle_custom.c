34bf2b6b45bb8eafc10fb94c7f0821b313adfb31
SherneVK <shernekinniar@gmail.com>
2023-09-01 05:42:24 -0700

    fix

--- handle_custom.c
+++ handle_custom.c
@@ -8,41 +8,40 @@
  */
 int handle_custom_string(va_list ap)
 {
-	char *str = va_arg(ap, char *);
-	int count = 0;
+    char *str = va_arg(ap, char *);
+    int count = 0;
 
-	char hex1, hex2;
+    if (str == NULL)
+    {
+        return (_printf("(null)"));
+    }
 
-	if (str == NULL)
-	{
-		return (_printf("(null)"));
-	}
+    for (; *str != '\0'; str++)
+    {
+        if (*str < 32 || *str >= 127)
+        {
+            count += _putchar('\\');
+            count += _putchar('x');
 
-	for (; *str != '\0'; str++)
-	{
-		if (*str < 32 || *str >= 127)
-		{
-			hex1 = (*str >> 4) & 0xF;
-			hex2 = *str & 0xF;
+            // Print the ASCII code value in hexadecimal (always 2 characters)
+            char hex1 = (*str >> 4) & 0xF;
+            char hex2 = *str & 0xF;
 
-			count += _putchar('\\');
-			count += _putchar('x');
+            if (hex1 < 10)
+                count += _putchar(hex1 + '0');
+            else
+                count += _putchar(hex1 - 10 + 'A');
 
-			if (hex1 < 10)
-				count += _putchar(hex1 + '0');
-			else
-				count += _putchar(hex1 - 10 + 'A');
+            if (hex2 < 10)
+                count += _putchar(hex2 + '0');
+            else
+                count += _putchar(hex2 - 10 + 'A');
+        }
+        else
+        {
+            count += _putchar(*str);
+        }
+    }
 
-			if (hex2 < 10)
-				count += _putchar(hex2 + '0');
-			else
-				count += _putchar(hex2 - 10 + 'A');
-		}
-		else
-		{
-			count += _putchar(*str);
-		}
-	}
-
-	return (count);
+    return (count);
 }
